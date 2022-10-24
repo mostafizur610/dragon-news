fd969edafd0c72a0c6ec32a276bcd83e7a4ce3a9
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-24 09:59:02 +0600

    set all category news show data

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCards from '../../Shared/NewsSummaryCards/NewsSummaryCards';
 
 const Category = () => {
+    const categoryNews = useLoaderData();
     return (
         <div>
-            <h2>This is Category</h2>
+            <h2>This is Category has news: {categoryNews.length}</h2>
+            {
+                categoryNews.map(news => <NewsSummaryCards
+                    key={news._id}
+                    news={news}
+                ></NewsSummaryCards>)
+            }
         </div>
     );
 };
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCards from '../../Shared/NewsSummaryCards/NewsSummaryCards';
 
 const Home = () => {
+    const allNews = useLoaderData();
     return (
         <div>
-            <h2>This is home Component</h2>
+            <h2>Dragon News Home: {allNews.length}</h2>
+            {
+                allNews.map(news => <NewsSummaryCards
+                    key={news._id}
+                    news={news}
+                ></NewsSummaryCards>)
+            }
         </div>
     );
 };
